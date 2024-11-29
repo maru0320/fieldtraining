d245afe69196a8f165322647c4bd9d4bd4ccc9f0
maru0320 <ur4m0804@naver.com>
2024-11-29 11:33:53 +0900

    Add files via upload

---
+++ main/java/com/springboot/relationship/data/entity/Producer.java
@@ -0,0 +1,42 @@
+package com.springboot.relationship.data.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@Table(name = "Producer")
+public class Producer extends BaseEntity{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String code;
+	
+	private String name;
+	
+	@ManyToMany
+	@ToString.Exclude
+	private List<Product> products = new ArrayList<>();
+	public void addProduct(Product product) {
+		products.add(product);
+	}
+
+}
---
+++ main/java/com/springboot/relationship/data/entity/ProductDetail.java
@@ -0,0 +1,32 @@
+package com.springboot.relationship.data.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "product_detail")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class ProductDetail extends BaseEntity{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String description;
+	@OneToOne
+	@JoinColumn(name = "product_number")
+	private Product product;
+}
