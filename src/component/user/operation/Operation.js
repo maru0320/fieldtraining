6417da8cd16038de21771dce5ea43c19523f6d37
maru0320 <ur4m0804@naver.com>
2024-12-02 17:35:00 +0900

    Delete src directory

--- src/component/user/operation/Operation.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Operation() {
-    return <h1>운영</h1>;
-  }
-
-export default Operation;
--- src/component/user/ratingcheck/RatingCheck.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function RatingCheck() {
-    return <h1>평가확인</h1>;
-  }
-
-export default RatingCheck;
