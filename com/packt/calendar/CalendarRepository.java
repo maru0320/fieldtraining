a34db7b1cab87cc79209172d036f46632c3fd2b1
jun_hyeok <133184162+hongjunhyeo@users.noreply.github.com>
2024-12-02 17:21:47 +0900

    Delete com/packt directory

--- com/packt/calendar/CalendarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.packt.fieldtraining.calendar.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.packt.fieldtraining.calendar.entity.Calendar;
-
-public interface CalendarRepository extends JpaRepository<Calendar, Long>{
-
-}
--- com/packt/managerinfo/ManagerInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.packt.fieldtraining.ManagerInfo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.packt.fieldtraining.common.entity.Manager;
-
-public interface ManagerInfoRepository extends JpaRepository<Manager, Long>{
-
-}
