3a99a3c8d628be1fc7237fa6146984b5d5adf367
jun_hyeok <133184162+hongjunhyeo@users.noreply.github.com>
2024-12-03 17:31:58 +0900

    Delete main/calendar directory

--- main/calendar/repository/CalendarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fieldtraining.calendar.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fieldtraining.calendar.entity.Calendar;
-
-public interface CalendarRepository extends JpaRepository<Calendar, Long>{
-	
-}
