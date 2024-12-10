b1650148ffe9ca0241b1301db525d6c84e91496c
maru0320 <ur4m0804@naver.com>
2024-12-10 11:12:22 +0900

    Delete maru directory

--- maru/com/fieldtraining/PracticeBoard/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fieldtraining.PracticeBoard.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fieldtraining.PracticeBoard.entity.Comment;
-
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
