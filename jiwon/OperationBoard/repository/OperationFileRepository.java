98710a948f04e84af4144aa49250dff1108acf59
JIWONCHOI52 <dbtjdwl12@gmail.com>
2024-12-11 13:43:40 +0900

    Delete jiwon/OperationBoard directory

--- jiwon/OperationBoard/repository/OperationFileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fieldtraining.OperationBoard.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fieldtraining.OperationBoard.entity.OperationFile;
-
-
-public interface OperationFileRepository extends JpaRepository<OperationFile, Long> {
-}
