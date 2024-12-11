088a0a0b58c75baf8f475d267393cce5985aca41
JIWONCHOI52 <dbtjdwl12@gmail.com>
2024-12-11 13:43:33 +0900

    Delete jiwon/ClassBoard directory

--- jiwon/ClassBoard/repository/ClassCommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fieldtraining.ClassBoard.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fieldtraining.ClassBoard.entity.ClassComment;
-
-
-public interface ClassCommentRepository extends JpaRepository<ClassComment, Long> {
-}
