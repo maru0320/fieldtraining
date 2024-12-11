8ea564fc22edada34e6edf648174a5f09808ae43
JIWONCHOI52 <dbtjdwl12@gmail.com>
2024-12-11 13:43:57 +0900

    Delete jiwon/UnionBoard directory

--- jiwon/UnionBoard/repository/UnionCommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fieldtraining.UnionBoard.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fieldtraining.UnionBoard.entity.UnionComment;
-
-
-public interface UnionCommentRepository extends JpaRepository<UnionComment, Long> {
-}
