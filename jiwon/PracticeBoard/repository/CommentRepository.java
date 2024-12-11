07226de9f34950e6459b6c094b46a613c3627dae
JIWONCHOI52 <dbtjdwl12@gmail.com>
2024-12-11 13:43:46 +0900

    Delete jiwon/PracticeBoard directory

--- jiwon/PracticeBoard/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fieldtraining.PracticeBoard.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fieldtraining.PracticeBoard.entity.PracticeComment;
-
-
-public interface CommentRepository extends JpaRepository<PracticeComment, Long> {
-}
