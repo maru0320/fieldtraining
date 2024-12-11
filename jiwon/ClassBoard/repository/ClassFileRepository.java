e70d39f4485bd7dec9d8dddbf375ee7f9a860688
JIWONCHOI52 <dbtjdwl12@gmail.com>
2024-12-11 15:21:43 +0900

    Delete jiwon directory

--- jiwon/ClassBoard/repository/ClassFileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fieldtraining.ClassBoard.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fieldtraining.ClassBoard.entity.ClassFile;
-
-
-public interface ClassFileRepository extends JpaRepository<ClassFile, Long> {
-}
--- jiwon/UnionBoard/repository/UnionFileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fieldtraining.UnionBoard.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fieldtraining.UnionBoard.entity.UnionFile;
-
-
-public interface UnionFileRepository extends JpaRepository<UnionFile, Long> {
-}
