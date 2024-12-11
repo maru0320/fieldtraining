c663e5879e5e9981bbf8291ecc02e6e41fc5283e
jun_hyeok <133184162+hongjunhyeo@users.noreply.github.com>
2024-12-11 12:44:27 +0900

    Update UserUpdateRepository.java

--- junhyeok/managerInfo/repository/UserUpdateRepository.java
+++ junhyeok/managerInfo/repository/UserUpdateRepository.java
@@ -7,6 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.fieldtraining.data.entity.User;
 
 public interface UserUpdateRepository extends JpaRepository<User, Long>{
-	// 사용자 ID로 사용자 조회
-    Optional<User> findByUserId(String userId);
+
 }
