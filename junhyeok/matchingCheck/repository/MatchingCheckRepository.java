11c1f832490f71990b3280b97585c112366f925f
jun_hyeok <133184162+hongjunhyeo@users.noreply.github.com>
2024-12-18 09:45:34 +0900

    Delete junhyeok/matchingCheck directory

--- junhyeok/matchingCheck/repository/MatchingCheckRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fieldtraining.matchingCheck.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fieldtraining.matching.entity.Match;
-
-public interface MatchingCheckRepository extends JpaRepository<Match, Long>{
-
-}
