c3c8d44f2a859279e9e7eaee6dc6a208779ada4f
jun_hyeok <133184162+hongjunhyeo@users.noreply.github.com>
2024-12-23 14:43:55 +0900

    Delete junhyeok/managerInfo directory

--- junhyeok/managerInfo/repository/UserUpdateRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fieldtraining.managerInfo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fieldtraining.data.entity.User;
-
-public interface UserUpdateRepository extends JpaRepository<User, Long>{
-}
